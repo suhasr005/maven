d676564982052ea578e3e76a42dd719dcc8fb43a
Suhas <Suhas@Suhas-PC>
2019-02-04 14:56:56 +0530

    first commit maven

---
+++ mavneweb/src/main/java/com/ssr/ser1.java
@@ -0,0 +1,43 @@
+package com.ssr;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ser1
+ */
+public class ser1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ser1() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+	 String name=request.getParameter("name");
+		PrintWriter pw=response.getWriter();
+		pw.print("hi");
+	}
+
+}
---
+++ mavneweb/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+<form action="ser1" method="post">
+Name:<input type="text" name="name">
+<input type="submit" value="servlet">
+</form>
+</html>
